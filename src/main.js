49d034543b7723754fb6b6d2121e6f7fa97182c3
HashimA-sudu <hashimpixeldev@gmail.com>
2025-11-07 14:54:26 +0300

    commit

--- src/main.js
+++ src/main.js
@@ -20,14 +20,14 @@ const loader = new GLTFLoader();
 loader.setDRACOLoader(dracoLoader);
 
 const TextureMap = {
-    FirstT: {day:"/textures/firstTextureFinal.webp" },
-    first:{day:"/textures/firstTextureFinal.webp" },
-    secondT:{day: "/textures/secondTextureFinal.webp"},
-    thirdT:{day: "/textures/thirdTextureSetFinal.webp"},
-    inanimate_thirdT:{day: "/textures/thirdTextureSetFinal.webp"},
-    leaf: {day: "/textures/leaf.png"},
-    collegeT: {day:"/textures/collegeSetFinal.webp"},
-    glass: {day:""},
+    FirstT: {day:"/textures/mapping/firstTextureFinal.webp" },
+    first:{day:"/textures/mapping/firstTextureFinal.webp" },
+    secondT:{day: "/textures/mapping/secondTextureFinal.webp"},
+    thirdT:{day: "/textures/mapping/thirdTextureSetFinal.webp"},
+    inanimate_thirdT:{day: "/textures/mapping/thirdTextureSetFinal.webp"},
+    leaf: {day: "/textures/mapping/leaf.png"},
+    collegeT: {day:"/textures/mapping/collegeSetFinal.webp"},
+    //glass: {day:""},
 };
 
 const environmentMap = new THREE.CubeTextureLoader();
@@ -41,6 +41,7 @@ const loadedTextures = {
 Object.entries(TextureMap).forEach(([key,paths])=>{
     const TextureDay = textureLoader.load(paths.day);
     TextureDay.flipY=false;
+    TextureDay.generateMipmaps = false;
     TextureDay.colorSpace = THREE.SRGBColorSpace
     loadedTextures.day[key] = TextureDay;
     
@@ -50,10 +51,50 @@ const scene = new THREE.Scene();
 scene.environment = envTexture;
 scene.background = envTexture;
 
+//materials
+const glassMaterial = new THREE.MeshPhysicalMaterial({
+                        transmission: 0.1,
+                        color: 0xffffff,
+                        transparent: true,
+                        opacity: 1,
+                        roughness: 0,
+                        metalness: 0,
+                        ior: 1.5,
+                        thickness: 0.01,
+                        reflectivity: 0.5,
+                        envMap: envTexture,
+                        envMapIntensity: 1,
+                        depthWrite: false,
+});
+
+
+//screen videos (gameMAN, monitor, tv screen, etc..)
+const VideoElement = document.createElement("video");
+VideoElement.src = "./textures/video/monitor.mp4";
+VideoElement.loop = true;
+VideoElement.muted = true;
+VideoElement.playsInline = true;
+VideoElement.autoplay = true;
+VideoElement.play();
+
+const VideoTexture = new THREE.VideoTexture(VideoElement);
+VideoTexture.colorSpace = THREE.SRGBColorSpace;
+VideoTexture.flipY = false;
+
+
 loader.load("/models/portfolio_compressed-models.glb", (glb)=>
 { 
     glb.scene.traverse(child=>{
+        
         if(child.isMesh){
+            if(child.name.includes("monitor_screen")) {
+                child.material = new THREE.MeshBasicMaterial({
+                   // map: VideoTexture,
+                });}    
+            else if(child.name.includes("glass")) {                    
+                    child.material = glassMaterial;
+            }
+
             // Debug: Log all mesh names to console
              if (!child.name.includes("leaf")){console.log("Mesh found:", child.name);}
             
@@ -63,36 +104,13 @@ loader.load("/models/portfolio_compressed-models.glb", (glb)=>
                         map: loadedTextures.day[keys],
                     });
                 
-                if (child.name.includes("hidden")) {
-                    const material = new THREE.MeshBasicMaterial({
-                        map: loadedTextures.day['leaf'],
-                });
-                }
+                
                 child.material = material;
 
                 if(child.material.map){ // fix distance thing
                     child.material.map.minFilter = THREE.LinearFilter;
                 }
                 
-        
-                if(child.name.includes("glass")) {
-                    // Move glass object up by 2 units on Z axis
-                    
-                    child.material = new THREE.MeshPhysicalMaterial({
-                        transmission: 0.2,
-                        transparent: true,
-                        opacity: 1,
-                        roughness: 0,
-                        metalness: 0,
-                        ior: 1.5,
-                        thickness: 0.02,
-                        envMap: envTexture,
-                        envMapIntensity: 1,
-                       
-                    });
-                 }
-
-
                 }
             });
         }
@@ -110,6 +128,9 @@ const camera = new THREE.PerspectiveCamera(
     sizes.width / sizes.height,
     0.1,
     1000);
+//default camera position
+camera.position.set(27.751215392472087,9.279443596422004, 8.730269582361316);
+
 
 const renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true});
 renderer.setSize( sizes.width, sizes.height );
@@ -120,6 +141,7 @@ camera.position.z = 5;
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.enableDamping = true;
 controls.dampingFactor = 0.05;
+controls.target.set(2.978904510135682,2.7952939833198513 ,-6.329682566513317); 
 
 // event listeners
 window.addEventListener("resize", ()=>{
@@ -134,6 +156,9 @@ window.addEventListener("resize", ()=>{
 
 const render = () =>{
     controls.update();
+    // console.log(camera.position);
+    // console.log("------");
+    // console.log(controls.target);
     renderer.render( scene, camera );
     window.requestAnimationFrame(render)
 }
